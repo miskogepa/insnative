bba0cf5cf76ba50e70430a8ed9fa183ac7155747
miskogepa <milovan.jankovic50m@gmail.com>
2025-08-07 13:05:57 +0200

    Refactor RootLayout to include SafeAreaProvider and SafeAreaView for improved layout handling. Update Index component to use Redirect for navigation instead of static content.

---
+++ app/(tabs)/boolmarks.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function boolmarks() {
+  return (
+    <View>
+      <Text>boolmarks</Text>
+    </View>
+  )
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View, } from "react-native";
+import { styles } from "../../styles/auth.styles";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={
+       styles.container
+      }
+    >
+      <Link href="/notifications">Notifications</Link>
+    </View>
+  );
+}
