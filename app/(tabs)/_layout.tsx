47142a20a463726548d3ed6813664a1b11bb57bc
miskogepa <milovan.jankovic50m@gmail.com>
2025-08-07 13:10:15 +0200

    Enhance TabsLayout by adding StatusBar for improved UI experience. Refactor tab structure for better readability while maintaining existing functionality.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,32 +1,71 @@
-import { Tabs } from "expo-router";
-import React from "react";
-import { Ionicons } from "@expo/vector-icons";
 import { COLORS } from "@/constants/theme";
+import { Ionicons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarShowLabel: false,
-        headerShown: false,
-        tabBarActiveTintColor: COLORS.primary,
-        tabBarInactiveTintColor: COLORS.grey,
-        tabBarStyle: {
-          backgroundColor: "black",
-          borderTopColor: "black",
-          borderTopWidth: 0,
-          position: "absolute",
-          elevation: 0,
-          height: 40,
-          paddingBottom: 8,
-        },
-      }}
-    >
-      <Tabs.Screen name="index" options={{ tabBarIcon: ({size,color}) => <Ionicons name="home" size={size} color={color} />}} />
-      <Tabs.Screen name="boolmarks" options={{ tabBarIcon: ({size,color}) => <Ionicons name="bookmark" size={size} color={color} />}} />
-      <Tabs.Screen name="create" options={{ tabBarIcon: ({size,color}) => <Ionicons name="add-circle" size={size} color={COLORS.primary} />}} />
-      <Tabs.Screen name="notifications" options={{ tabBarIcon: ({size,color}) => <Ionicons name="notifications" size={size} color={color} />}} />
-      <Tabs.Screen name="profile" options={{ tabBarIcon: ({size,color}) => <Ionicons name="person" size={size} color={color} />}} />
-    </Tabs>
+    <>
+      <StatusBar style="dark" />
+      <Tabs
+        screenOptions={{
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarActiveTintColor: COLORS.primary,
+          tabBarInactiveTintColor: COLORS.grey,
+          tabBarStyle: {
+            backgroundColor: "black",
+            borderTopColor: "black",
+            borderTopWidth: 0,
+            position: "absolute",
+            elevation: 0,
+            height: 40,
+            paddingBottom: 8,
+          },
+        }}
+      >
+        <Tabs.Screen
+          name="index"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="boolmarks"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Ionicons name="bookmark" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="create"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Ionicons name="add-circle" size={size} color={COLORS.primary} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="notifications"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Ionicons name="notifications" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="profile"
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tabs>
+    </>
   );
 }
